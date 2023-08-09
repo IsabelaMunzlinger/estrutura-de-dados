269f7193afff742345bf01bf318fa8a6efdf92e9
Guilherme Anzollin <guilherme.anzollin@codelabore.com>
2023-08-08 22:03:07 -0300

    Atividade 3 - Part 1

---
+++ atividade-3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+typedef struct{
+    float altura;
+    float largura;
+    float profundidade;
+} REGISTRO;
+
+int main () {
+
+    REGISTRO registros[10];
+
+    // menu de opções
+    // i (índices) de 0 a 9
+    // registros[i].altura
+    // registros[i].largura
+    // registros[i].profundidade
+}
