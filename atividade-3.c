513a22f99da5785d70b97c127697077850fcd942
Guilherme Anzollin <guilherme.anzollin@codelabore.com>
2023-08-09 21:57:51 -0300

    Atividade 3

--- atividade-3.c
+++ atividade-3.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-typedef struct{
+typedef struct {
     float altura;
     float largura;
     float profundidade;
@@ -9,10 +9,64 @@ typedef struct{
 int main () {
 
     REGISTRO registros[10];
+    int opcao, posicao;
 
-    // menu de opções
-    // i (índices) de 0 a 9
-    // registros[i].altura
-    // registros[i].largura
-    // registros[i].profundidade
+    for (int i = 0; i < 10; i++) {
+        registros[i].altura = 0;
+        registros[i].largura = 0;
+        registros[i].profundidade = 0;
+    }
+
+    do {
+        puts("1 - Inserir registro");
+        puts("2 - Alterar registro");
+        puts("3 - Excluir registro");
+        puts("4 - Mostrar registros");
+        puts("5 - Sair");
+        scanf("%i", &opcao);
+
+        switch (opcao) {
+        case 1:
+            puts("Informe uma posicao (0-9)");
+            scanf("%i", &posicao);
+            puts("Informe a altura");
+            scanf("%f", &registros[posicao].altura);
+            puts("Informe a largura");
+            scanf("%f", &registros[posicao].largura);
+            puts("Informe a profundidade");
+            scanf("%f", &registros[posicao].profundidade);
+            break;
+
+        case 2:
+            puts("Informe uma posicao (0-9)");
+            scanf("%i", &posicao);
+            puts("Informe a altura");
+            scanf("%f", &registros[posicao].altura);
+            puts("Informe a largura");
+            scanf("%f", &registros[posicao].largura);
+            puts("Informe a profundidade");
+            scanf("%f", &registros[posicao].profundidade);
+            break;
+
+        case 3:
+            puts("Informe uma posicao (0-9)");
+            scanf("%i", &posicao);
+            registros[posicao].altura = 0;
+            (*(registros+posicao)).largura = 0;
+            (registros+posicao)->profundidade = 0;
+            break;
+
+        case 4:
+            for (int i = 0; i < 10; i++) {
+                printf("Altura: %.2f ", registros[i].altura);
+                printf("Largura: %.2f ", registros[i].largura);
+                printf("Profundidade: %.2f \n", registros[i].profundidade);
+            }
+            break;
+
+        default:
+            break;
+        }
+
+    } while (opcao != 5);
 }
